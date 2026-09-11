ff258f2f2eda55fa03017f23071d18b730b4e680
Nishchal Gautam <gautam.nishchal@gmail.com>
2026-09-11 18:32:23 +0400

    ci: setup release candidates

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     branches: [
         'main',
-        {name: '!(main)', prerelease: 'rc'},
+        {name: ['**', '!main'], prerelease: 'rc'},
     ],
     verifyConditions: [
         "@semantic-release/github"
